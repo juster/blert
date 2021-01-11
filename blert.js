a0e1dbdc4ec91e4fb08d2356e6a4d6bf351834f9
Justin Davis <jrcd83@gmail.com>
2021-01-11 09:32:27 -0500

    Forgot to not export isNil.

--- blert.js
+++ blert.js
@@ -7,8 +7,7 @@
 
 var exports = {
     alloc: alloc, encode: encode, decode: decode,
-    isAtom: isAtom, isTuple: isTuple, isList: isList,
-    isProper: isProper, isNil: isNil
+    isAtom: isAtom, isTuple: isTuple, isList: isList, isProper: isProper
 }
 if(typeof module !== "undefined"){
     module.exports = exports
@@ -424,11 +423,11 @@ function decode(A){
 }
 
 function isAtom(A){
-    return (typeof A === "object") && ("atom" in A)
+    return typeof A === "object" && "atom" in A
 }
 
 function isTuple(T){
-    return (typeof T === "object" && "tuple" in T)
+    return typeof T === "object" && "tuple" in T
 }
 
 function isList(L){
